b8684d489fe07e3e104208bb82201928f87e4cba
orviz <orviz@ifca.unican.es>
2023-11-16 14:24:53 +0100

    Add JePL file structure

--- .sqa/script.bluesy-smalt-urchin.sh
+++ .sqa/script.bluesy-smalt-urchin.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/eosc-synergy/sqaaas-assessment-action &&
-    git rev-parse --is-inside-work-tree
+cd github.com/EOSC-synergy/sqaaas-assessment-action &&
+    get_git_tags.py
 )
